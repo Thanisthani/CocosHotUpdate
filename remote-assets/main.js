0b5e83e2a7fc950919f19175573bf0a621b735fc
Thanisthani <thanithanis171198@gmail.com>
2025-06-12 17:18:49 +0530

    updated

--- remote-assets/main.js
+++ remote-assets/main.js
@@ -2,31 +2,19 @@
 
 // SystemJS support.
 window.self = window;
-require("src/system.bundle.f45da.js");
+require("src/system.bundle.js");
 
-if (jsb && jsb.fileUtils && jsb.localStorage) {
-    const hotUpdatePath = jsb.localStorage.getItem('hotUpdatePath');
-    if (hotUpdatePath && jsb.fileUtils.isDirectoryExist(hotUpdatePath)) {
-        const searchPaths = jsb.fileUtils.getSearchPaths();
-        if (!searchPaths.includes(hotUpdatePath)) {
-            searchPaths.unshift(hotUpdatePath);
-            jsb.fileUtils.setSearchPaths(searchPaths);
-        }
-        console.log("Hot update search paths applied:", searchPaths);
-    }
-  }
-  
-const importMapJson = jsb.fileUtils.getStringFromFile("src/import-map.96fa8.json");
+const importMapJson = jsb.fileUtils.getStringFromFile("src/import-map.json");
 const importMap = JSON.parse(importMapJson);
 System.warmup({
     importMap,
-    importMapUrl: 'src/import-map.96fa8.json',
+    importMapUrl: 'src/import-map.json',
     defaultHandler: (urlNoSchema) => {
         require(urlNoSchema.startsWith('/') ? urlNoSchema.substr(1) : urlNoSchema);
     },
 });
 
-System.import('./application.86f6c.js')
+System.import('./application.js')
 .then(({ Application }) => {
     return new Application();
 }).then((application) => {
